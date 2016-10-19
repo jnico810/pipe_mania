50702f790581708872d3ee9d1888d0d1ba3173d1
JOHN NICO <jnico810@gmail.com>
2016-10-19 16:24:18 -0400

    Right before big overhau;

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/game.js",
+  output: {
+    path: "./lib",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
