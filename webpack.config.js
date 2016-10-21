86d372863ce804a9a7b9fecf80669fb1ad51bae3
JOHN NICO <jnico810@gmail.com>
2016-10-21 18:40:08 -0400

    Styling, about to add spill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./lib/game.js",
+  entry: "./lib/sewer.js",
   output: {
     path: "./lib",
     filename: "bundle.js"
